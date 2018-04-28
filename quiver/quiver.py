ad6623ac1c5bca009c361280e799d714bdb720f4
Juan Antonio Aguilar <juanantonioaguilar@gmail.com>
2018-04-28 20:52:02 +0200

    comment fixed

--- quiver/quiver.py
+++ quiver/quiver.py
@@ -324,7 +324,7 @@ def _map_stack(h_row, stack_p_key, primary_key):
             # from the original dataset, so it must be part of the key to join associated elements
             # to the previous dataset's key plus column index
             quiver_pair_=str(uuid3(NAMESPACE_URL, '{}_{}'.format(str(stack_p_key_value), indx))),
-            # filter prefix, normally in development with large URLs
+            # different column values, _trim_str: filter prefix, normally in development with large URLs
             quiver_column_=_trim_str(val)
         ) for indx, val in enumerate(h_row[len(primary_key):])  # iterates over data columns
     ]
